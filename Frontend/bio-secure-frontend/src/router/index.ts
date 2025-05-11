1db04404f3dc87f944fd23b8a6eded7165cde9e2
anuphat manotam <anuphat_m@hotmail.com>
2025-05-12 04:10:36 +0700

    add the page identify, register and monitor page
    also add the detail in the register page

--- Frontend/bio-secure-frontend/src/router/index.ts
+++ Frontend/bio-secure-frontend/src/router/index.ts
@@ -2,12 +2,30 @@
 
 import { createRouter, createWebHistory} from 'vue-router'
 import MainMenu from '../view/MainMenu.vue'
+import Register from '../view/Register.vue'
+import Identify from '../view/Identify.vue'
+import Monitor from '../view/Monitor.vue'
 
 const routes = [
   {
     path: '/',
     name: 'mainmenu',
     component: MainMenu
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: Register
+  },
+  {
+    path: '/identify',
+    name: 'identify',
+    component: Identify
+  },
+  {
+    path: '/monitor',
+    name: 'monitor',
+    component: Monitor
   },
 ]
 
