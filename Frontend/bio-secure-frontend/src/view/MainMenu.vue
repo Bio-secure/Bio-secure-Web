e1999a925e7448f4529c43a951a8b8761de9d418
anuphat manotam <anuphat_m@hotmail.com>
2025-05-11 23:37:20 +0700

    Add the mainmenu

--- Frontend/bio-secure-frontend/src/view/MainMenu.vue
+++ Frontend/bio-secure-frontend/src/view/MainMenu.vue
@@ -1,3 +1,35 @@
 <template>
-    
+  <div class="min-h-screen bg-gray-200 flex items-center justify-center">
+    <div class="grid grid-cols-3 gap-28">
+      <!-- Register -->
+      <div class="p-10 flex flex-col items-center">
+        <img src="../assets/Register.png" alt="Register" class="w-60 h-60 mb-8" />
+        <RouterLink to="/register">
+          <button class="mt-6 bg-blue-300 text-black font-bold text-2xl px-8 py-4 rounded-2xl shadow-md hover:bg-blue-400">
+            REGISTER
+          </button>
+        </RouterLink>
+      </div>
+
+      <!-- Identify -->
+      <div class="p-10 flex flex-col items-center">
+        <img src="../assets/Identify.png" alt="Identify" class="w-60 h-60 mb-8" />
+        <RouterLink to="/identify">
+          <button class="mt-6 bg-blue-300 text-black font-bold text-2xl px-8 py-4 rounded-2xl shadow-md hover:bg-blue-400">
+            IDENTIFY
+          </button>
+        </RouterLink>
+      </div>
+
+      <!-- Dashboard -->
+      <div class="p-10 flex flex-col items-center">
+        <img src="../assets/Register.png" alt="Dashboard" class="w-60 h-60 mb-8" />
+        <RouterLink to="/dashboard">
+          <button class="mt-6 bg-blue-300 text-black font-bold text-2xl px-8 py-4 rounded-2xl shadow-md hover:bg-blue-400">
+            DASHBOARD
+          </button>
+        </RouterLink>
+      </div>
+    </div>
+  </div>
 </template>
