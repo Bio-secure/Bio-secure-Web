bb8d26e0fdbcf1ac30dee73185cd7089e9fa0684
PhilipHousdens <philip.houden.04@gmail.com>
2025-08-16 21:05:08 +0700

    created the Account Management Page

---
+++ Frontend/bio-secure-frontend/src/view/AccountMag.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    name: 'AccountMag'
+}
+</script>
+
+
+<template>
+    <div class="p-8 text-center">
+        <h1 class="text-3xl font-bold mb-4">Account Management</h1>
+        <p class="text-lg text-gray-600">Welcome to the account management page.</p>
+    </div>
+</template>
+
