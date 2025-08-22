3ba9d0305dfb2d72cd53752908afa0d982684840
PhilipHousdens <philip.houden.04@gmail.com>
2025-08-22 19:13:38 +0700

    Match account manage page with new style

--- Frontend/bio-secure-frontend/src/view/AccountMag.vue
+++ Frontend/bio-secure-frontend/src/view/AccountMag.vue
@@ -32,7 +32,7 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="p-6">
+  <div class="p-6 h-screen bg-slate-100 font-sans">
     <!-- Loading -->
     <div v-if="loading" class="text-gray-500">Loading customers...</div>
 
@@ -42,61 +42,68 @@ onMounted(async () => {
     </div>
 
     <!-- Customer Table -->
+    
     <div v-else>
-      <h2 class="text-2xl font-bold mb-4">Customer List</h2>
-      <div class="overflow-x-auto">
-        <table class="min-w-full bg-white border border-gray-200 rounded-lg shadow">
-          <thead class="bg-gray-100 text-gray-700">
-            <tr>
-              <th class="py-2 px-4 text-left">ID</th>
-              <th class="py-2 px-4 text-left">First Name</th>
-              <th class="py-2 px-4 text-left">Last Name</th>
-              <th class="py-2 px-4 text-left">Phone No.</th>
-              <th class="py-2 px-4 text-left">Email</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr
-              v-for="c in customers"
-              :key="c.National_ID"
-              class="border-t hover:bg-gray-50"
-            >
-              <td class="py-2 px-4">{{ c.National_ID }}</td>
-              <td class="py-2 px-4">{{ c.Name }}</td>
-              <td class="py-2 px-4">{{ c.SurName }}</td>
-              <td class="py-2 px-4">{{ c.phone_no }}</td>
-              <td class="py-2 px-4">{{ c.Email }}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+        <div class="w-[80%] lg:col-span-2 mx-auto bg-white rounded-2xl shadow-md p-6 flex flex-col">
+            <h2 class="text-2xl font-semibold text-gray-800 pb-5">Customer List</h2>
+            <div class=" overflow-y-auto h-auto border-t border-gray-100 ">
+                <table class=" w-full text-sm">
+                    <thead class="bg-gray-100 text-gray-700">
+                            <tr>
+                                <th class="py-2 px-4 text-left">ID</th>
+                                <th class="py-2 px-4 text-left">First Name</th>
+                                <th class="py-2 px-4 text-left">Last Name</th>
+                                <th class="py-2 px-4 text-left">Phone No.</th>
+                                <th class="py-2 px-4 text-left">Email</th>
+                            </tr>
+                    </thead>
+                    <tbody>
+                        <tr
+                        v-for="c in customers"
+                        :key="c.National_ID"
+                        class="border-b border-gray-100 hover:bg-gray-50"
+                        >
+                            <td class="py-2 px-4">{{ c.National_ID }}</td>
+                            <td class="py-2 px-4">{{ c.Name }}</td>
+                            <td class="py-2 px-4">{{ c.SurName }}</td>
+                            <td class="py-2 px-4">{{ c.phone_no }}</td>
+                            <td class="py-2 px-4">{{ c.Email }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+      
       <br>
-      <h2 class="text-2xl font-bold mb-4">
-        Employee List
-      </h2>
-      <div class="overflow-x-auto">
-        <table class="min-w-full bg-white border border-gray-200 rounded-lg shadow">
-          <thead class="bg-gray-100 text-gray-700">
-            <tr>
-              <th class="py-2 px-4 text-left">ID</th>
-              <th class="py-2 px-4 text-left">First Name</th>
-              <th class="py-2 px-4 text-left">Last Name</th>
-              <th class="py-2 px-4 text-left">Is Admin</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr
-              v-for="e in employees"
-              :key="e.EmID"
-              class="border-t hover:bg-gray-50"
-            >
-              <td class="py-2 px-4">{{ e.EmID }}</td>
-              <td class="py-2 px-4">{{ e.EmName }}</td>
-              <td class="py-2 px-4">{{ e.EmSurName }}</td>
-              <td class="py-2 px-4">{{ e.IsAdmin }}</td>
-            </tr>
-          </tbody>
-        </table>
+        <!-- Employee Table -->
+      <div class="w-[80%] lg:col-span-2 mx-auto bg-white rounded-2xl shadow-md p-6 flex flex-col">
+        <h2 class="text-2xl font-semibold text-gray-800 pb-5">
+            Employee List
+        </h2>
+        <div class=" overflow-y-auto h-auto border-t border-gray-100 ">
+            <table class=" w-full text-sm">
+                <thead class="bg-gray-100 text-gray-700">
+                    <tr>
+                    <th class="py-2 px-4 text-left">ID</th>
+                    <th class="py-2 px-4 text-left">First Name</th>
+                    <th class="py-2 px-4 text-left">Last Name</th>
+                    <th class="py-2 px-4 text-left">Is Admin</th>
+                    </tr>
+                </thead>
+                <tbody>
+                        <tr
+                        v-for="e in employees"
+                        :key="e.EmID"
+                        class="border-b border-gray-100 hover:bg-gray-50"
+                        >
+                            <td class="py-2 px-4">{{ e.EmID }}</td>
+                            <td class="py-2 px-4">{{ e.EmName }}</td>
+                            <td class="py-2 px-4">{{ e.EmSurName }}</td>
+                            <td class="py-2 px-4">{{ e.IsAdmin }}</td>
+                        </tr>
+                </tbody>
+            </table>
+        </div>
       </div>
     </div>
   </div>
